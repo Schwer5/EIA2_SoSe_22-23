e68c4b922829a47f37c0205deeca25a21248c008
Schwer5 <115712200+Schwer5@users.noreply.github.com>
2023-03-25 19:23:59 +0100

    for und function hinzufuegen

--- A01_randompoem/randompoem.js
+++ A01_randompoem/randompoem.js
@@ -1,2 +1,24 @@
 "use strict";
+var randompoem;
+(function (randompoem) {
+    let subjects = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let verbs = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    //console.log(subjects,verbs,objects);
+    for (let index = verbs.length; index > 0; index--) {
+        getVerse(subjects, verbs, objects);
+    }
+    function getVerse(subject, verb, object) {
+        let randomsubject = Math.floor(Math.random() * subjects.length);
+        let randomverb = Math.floor(Math.random() * verbs.length);
+        let randomobject = Math.floor(Math.random() * objects.length);
+        let verse = subjects[randomsubject] + " " + verbs[randomverb] + " " + objects[randomobject];
+        subject.splice(randomsubject, 1);
+        verb.splice(randomverb, 1);
+        object.splice(randomobject, 1);
+        console.log(verse);
+        return verse;
+    }
+    ;
+})(randompoem || (randompoem = {}));
 //# sourceMappingURL=randompoem.js.map
--- A01_randompoem/randompoem.ts
+++ A01_randompoem/randompoem.ts
@@ -1,14 +1,32 @@
 namespace randompoem {
 
-let subjects: string[]=["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"]
-let verbs: string[]=["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"]
-let objects: string[]=["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"]
+let subjects: string[]=["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
 
-console.log(subjects);
-console.log(verbs);
-console.log(objects);
+let verbs: string[]=["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+let objects: string[]=["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+
+//console.log(subjects,verbs,objects);
 
 
+for (let index = verbs.length; index > 0; index--) {
+    getVerse(subjects, verbs, objects);
+}
+
+function getVerse(subject: string[], verb: string[], object: string[]): string {
+    let randomsubject: number = Math.floor(Math.random() * subjects.length);
+    let randomverb: number = Math.floor(Math.random() * verbs.length);
+    let randomobject: number = Math.floor(Math.random() * objects.length);
+    
+    let verse: string = subjects[randomsubject] + " " + verbs[randomverb] + " " + objects[randomobject];
+
+    subject.splice(randomsubject, 1);
+    verb.splice(randomverb, 1);
+    object.splice(randomobject, 1);
+
+    console.log(verse);
+    return verse;
+
+};
 
 
 
