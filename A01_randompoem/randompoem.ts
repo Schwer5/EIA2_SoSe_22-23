2925b33dbb98a541075c686775047d622f225dae
Schwer5 <115712200+Schwer5@users.noreply.github.com>
2023-03-25 18:56:05 +0100

    Update randompoem.ts

--- A01_randompoem/randompoem.ts
+++ A01_randompoem/randompoem.ts
@@ -4,9 +4,9 @@ let subjects: string[]=["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledor
 let verbs: string[]=["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"]
 let objects: string[]=["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"]
 
-console.log(subjects);
-console.log(verbs);
-console.log(objects);
+//console.log(subjects);
+//console.log(verbs);
+//console.log(objects);
 
 
 
