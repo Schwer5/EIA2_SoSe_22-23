739e4edcf9a6913fbd9058bd6f9d1a026d44541d
Schwer5 <115712200+Schwer5@users.noreply.github.com>
2023-03-25 18:41:55 +0100

    verbs hinzugefügt

---
+++ A01_randompoem/randompoem.ts
@@ -0,0 +1,29 @@
+namespace randompoem {
+
+let subjects: string[]=["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"]
+let verbs: string[]=["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
