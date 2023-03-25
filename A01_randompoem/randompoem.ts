7f4abe65851e2cd9046fbad080ebd488ff5437b6
Schwer5 <115712200+Schwer5@users.noreply.github.com>
2023-03-25 20:17:55 +0100

    aktivitätsdiagramme

--- A01_randompoem/randompoem.ts
+++ A01_randompoem/randompoem.ts
@@ -1,7 +1,6 @@
 namespace randompoem {
 
 let subjects: string[]=["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-
 let verbs: string[]=["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
 let objects: string[]=["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
 
