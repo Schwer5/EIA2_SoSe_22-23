ff602ad02fde5a343b3efc5d78f1c3a3a06675d8
Schwer5 <115712200+Schwer5@users.noreply.github.com>
2023-03-25 18:44:44 +0100

    objects hinzufuegen und alles ausgeben

--- A01_randompoem/randompoem.ts
+++ A01_randompoem/randompoem.ts
@@ -2,7 +2,11 @@ namespace randompoem {
 
 let subjects: string[]=["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"]
 let verbs: string[]=["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"]
+let objects: string[]=["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"]
 
+console.log(subjects)
+console.log(verbs)
+console.log(objects)
 
 
 
