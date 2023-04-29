3389d51910bc8c2376697417165b6ff385a82481
Schwer5 <115712200+Schwer5@users.noreply.github.com>
2023-04-29 09:54:13 +0200

    Falls etwas schief läuft: hierher zurück

---
+++ A06_Aufgabenliste_Formular/A06_Aufgabenliste_Formular.js
@@ -0,0 +1,96 @@
+"use strict";
+var A05_Aufgabenliste_Formular;
+(function (A05_Aufgabenliste_Formular) {
+    /*
+     Aufgabe: <Aufgabe 05 Aufgabenliste_Formular>
+     Name:<Pia Schwer>
+     Matrikel: <272266>
+     Datum: <15.04.23>
+     Zusammenarbeit mit Theresa Hauser, Marie Eckl
+     Quellen: Stack Overflow, Developer Mozilla,Github Jirka, Vorherige Aufgabe(n) aus EIA1
+     */
+    window.addEventListener('load', handleLoad);
+    let data = [];
+    function handleLoad() {
+        let addtask = document.querySelector('#addtask');
+        addtask.addEventListener('click', logaddtask);
+        console.log(data);
+        loaddata();
+    }
+    function logaddtask() {
+        const inputTodo = document.querySelector('#inputTodo');
+        const inputValue = inputTodo.value;
+        const inputComment = document.querySelector('#inputComment');
+        const commentValue = inputComment.value;
+        const selectName = document.querySelector('#selectname');
+        const nameValue = selectName.value;
+        const datetime = document.querySelector('#datetime');
+        const dateValue = datetime.value;
+        let newid = 1;
+        while (-1 != data.findIndex(function (item) { return item.id === newid; })) {
+            newid = newid + 1;
+        }
+        const newItem = {
+            id: newid,
+            title: inputValue,
+            comment: commentValue,
+            name: nameValue,
+            date: dateValue,
+            status: false,
+        };
+        data.push(newItem);
+        createtask(newItem);
+        inputTodo.value = '';
+        inputComment.value = '';
+        selectName.value = '';
+        datetime.value = '';
+    }
+    async function createtask(item) {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('inputtask');
+        newDiv.innerHTML = `
+            <input type="radio" id="edit" name="edit" value="HTML">
+            <label for="edit">In Bearbeitung</label><br>
+            <input type="text" id="inputTodo" placeholder="${item.title}">
+            <button id="deletetask"><i class="fas fa-trash"></i></button>
+            <input type="text" id="inputComment" placeholder="${item.comment}">
+            <select name="Name" id="selectname">
+                <option value="" selected>${item.name}</option>
+            </select>
+            <input type="datetime-local" name="date" id="datetime" placeholder="${item.date}">
+        `;
+        let query = new URLSearchParams();
+        query.set("command", "insert");
+        query.set("collection", "TaskList");
+        query.set("data", JSON.stringify(json));
+        await fetch("https://webuser.hs-furtwangen.de/~schwerpi/Database/");
+        let deleteButton = newDiv.querySelector('#deletetask');
+        if (deleteButton) {
+            deleteButton.addEventListener('click', function (event) {
+                deletetaskdom(event);
+                deletetask(item.id);
+            });
+        }
+        let container = document.querySelector('#task-container');
+        container && container.appendChild(newDiv);
+    }
+    async function loaddata() {
+        let response = await fetch('https://webuser.hs-furtwangen.de/~schwerpi/Database/TaskList.json');
+        let task = await response.text();
+        let dataJSON = JSON.parse(task);
+        data = dataJSON.data;
+        data.forEach(function (item) {
+            createtask(item);
+        });
+    }
+    function deletetask(id) {
+        let index = data.findIndex(function (item) { return item.id === id; });
+        data.splice(index, 1);
+    }
+    function deletetaskdom(event) {
+        const target = event.target;
+        const divToDelete = target.closest('div');
+        divToDelete && divToDelete.remove();
+    }
+})(A05_Aufgabenliste_Formular || (A05_Aufgabenliste_Formular = {}));
+//# sourceMappingURL=A06_Aufgabenliste_Formular.js.map
---
+++ A06_Aufgabenliste_Formular/A06_Aufgabenliste_Formular.ts
@@ -0,0 +1,125 @@
+namespace A05_Aufgabenliste_Formular {
+    /*
+     Aufgabe: <Aufgabe 05 Aufgabenliste_Formular>
+     Name:<Pia Schwer>
+     Matrikel: <272266>
+     Datum: <15.04.23>
+     Zusammenarbeit mit Theresa Hauser, Marie Eckl
+     Quellen: Stack Overflow, Developer Mozilla,Github Jirka, Vorherige Aufgabe(n) aus EIA1
+     */
+    window.addEventListener('load', handleLoad);
+    interface Item {
+        id: number;
+        title: string;
+        comment: string;
+        name: string;
+        date: string;
+        status: boolean;
+    }
+    let data: Item[] = [];
+
+    function handleLoad() {
+        let addtask: HTMLElement = <HTMLElement>document.querySelector('#addtask');
+        addtask.addEventListener('click', logaddtask);
+        console.log(data);
+        loaddata();
+    }
+
+    function logaddtask(): void {
+        const inputTodo = document.querySelector('#inputTodo') as HTMLInputElement;
+        const inputValue = inputTodo.value;
+
+        const inputComment = document.querySelector('#inputComment') as HTMLInputElement;
+        const commentValue = inputComment.value;
+
+        const selectName = document.querySelector('#selectname') as HTMLSelectElement;
+        const nameValue = selectName.value;
+
+        const datetime = document.querySelector('#datetime') as HTMLInputElement;
+        const dateValue = datetime.value;
+
+        let newid = 1
+        while (-1 != data.findIndex(function (item) { return item.id === newid })) {
+            newid = newid + 1
+        }
+        const newItem: Item = {
+            id: newid,
+            title: inputValue,
+            comment: commentValue,
+            name: nameValue,
+            date: dateValue,
+            status: false,
+        };
+
+        data.push(newItem);
+
+        createtask(newItem);
+
+        inputTodo.value = '';
+        inputComment.value = '';
+        selectName.value = '';
+        datetime.value = '';
+    }
+
+
+   async function createtask(item: Item): Promise<void> {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('inputtask')
+        newDiv.innerHTML = `
+            <input type="radio" id="edit" name="edit" value="HTML">
+            <label for="edit">In Bearbeitung</label><br>
+            <input type="text" id="inputTodo" placeholder="${item.title}">
+            <button id="deletetask"><i class="fas fa-trash"></i></button>
+            <input type="text" id="inputComment" placeholder="${item.comment}">
+            <select name="Name" id="selectname">
+                <option value="" selected>${item.name}</option>
+            </select>
+            <input type="datetime-local" name="date" id="datetime" placeholder="${item.date}">
+        `;
+        let query: URLSearchParams = new URLSearchParams();
+        query.set("command", "insert");
+        query.set("collection", "TaskList");
+        query.set("data", JSON.stringify(json));
+        await fetch ("https://webuser.hs-furtwangen.de/~schwerpi/Database/")
+
+
+
+        let deleteButton = newDiv.querySelector('#deletetask');
+        if (deleteButton) {
+            deleteButton.addEventListener('click', function (event) {
+                deletetaskdom(event);
+                deletetask(item.id);
+            });
+        }
+
+        let container = document.querySelector('#task-container');
+        container && container.appendChild(newDiv);
+    }
+
+
+    async function loaddata(): Promise<void> {
+        let response: Response = await fetch('https://webuser.hs-furtwangen.de/~schwerpi/Database/TaskList.json');
+        let task: string = await response.text();
+        let dataJSON: any = JSON.parse(task);
+        data = dataJSON.data;
+
+        data.forEach(function (item) {
+            createtask(item);
+        });
+    }
+
+
+    function deletetask(id: number): void {
+        let index = data.findIndex(function (item) { return item.id === id });
+        data.splice(index, 1);
+    }
+
+    function deletetaskdom(event: Event): void {
+        const target = event.target as HTMLElement;
+        const divToDelete = target.closest('div');
+        divToDelete && divToDelete.remove();
+    }
+
+}
+
+
