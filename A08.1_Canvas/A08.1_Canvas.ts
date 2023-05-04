49f8b32a265d1ff394cd591b28f2e23483496f60
Schwer5 <115712200+Schwer5@users.noreply.github.com>
2023-05-04 10:11:04 +0200

    fertig

--- A08.1_Canvas/A08.1_Canvas.ts
+++ A08.1_Canvas/A08.1_Canvas.ts
@@ -2,39 +2,39 @@ namespace A081 {
     window.addEventListener("load", handleLoad);
 
 function handleLoad(){
-        const canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
-        const ctx = canvas.getContext("2d")!;
+        let canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+        let context = canvas.getContext("2d")!;
 
         function randomInt(min: number, max: number): number {
             return Math.floor(Math.random() * (max - min + 1) + min);
         }
 
         function drawRandomLine() {
-            ctx.beginPath();
-            ctx.moveTo(randomInt(0, canvas.width), randomInt(0, canvas.height));
-            ctx.lineTo(randomInt(0, canvas.width), randomInt(0, canvas.height));
-            ctx.strokeStyle = `rgba(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)}, ${Math.random()})`;
-            ctx.lineWidth = randomInt(1, 10);
-            ctx.stroke();
+            context.beginPath();
+            context.moveTo(randomInt(0, canvas.width), randomInt(0, canvas.height));
+            context.lineTo(randomInt(0, canvas.width), randomInt(0, canvas.height));
+            context.strokeStyle = `rgba(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)}, ${Math.random()})`;
+            context.lineWidth = randomInt(1, 10);
+            context.fill();
         }
 
         function drawRandomCircle() {
-            ctx.beginPath();
-            ctx.arc(randomInt(0, canvas.width), randomInt(0, canvas.height), randomInt(1, 100), 0, 2 * Math.PI);
-            ctx.fillStyle = `rgba(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)}, ${Math.random()})`;
-            ctx.fill();
+            context.beginPath();
+            context.arc(randomInt(0, canvas.width), randomInt(0, canvas.height), randomInt(1, 100), 0, 2 * Math.PI);
+            context.fillStyle = `rgba(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)}, ${Math.random()})`;
+            context.fill();
         }
 
         function drawRandomRectangle() {
-            ctx.beginPath();
-            ctx.rect(randomInt(0, canvas.width), randomInt(0, canvas.height), randomInt(1, 150), randomInt(1, 150));
-            ctx.fillStyle = `rgba(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)}, ${Math.random()})`;
-            ctx.fill();
+            context.beginPath();
+            context.rect(randomInt(0, canvas.width), randomInt(0, canvas.height), randomInt(1, 150), randomInt(1, 150));
+            context.fillStyle = `rgba(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)}, ${Math.random()})`;
+            context.fill();
         }
 
         function generateArt() {
             for (let i = 0; i < 100; i++) {
-                const shape = randomInt(1, 3);
+                let shape = randomInt(1, 3);
 
                 if (shape === 1) {
                     drawRandomLine();
